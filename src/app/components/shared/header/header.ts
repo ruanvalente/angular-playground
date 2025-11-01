c11c5403b050b57c0e5030b9db6f7bc0e892fa08
ruanvalente <ruanopensuselinux@gmail.com>
2025-11-01 11:07:39 -0300

    refactor: simplify header component by removing unused router logic

--- src/app/components/shared/header/header.ts
+++ src/app/components/shared/header/header.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,6 +8,4 @@ import { CommonModule } from '@angular/common';
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {
-  protected readonly hasRouterRepository = input<boolean>(false);
-}
+export class Header {}
