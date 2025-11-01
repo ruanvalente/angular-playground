aa3ff558adf95d215bc2a23f04e1549b052d78e3
ruanvalente <ruanopensuselinux@gmail.com>
2025-11-01 11:16:44 -0300

    fix: add server routes for repository and issue details

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'repository/:owner/:repo',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'repository/:owner/:repo/issues/:number',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
