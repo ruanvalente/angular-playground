637db96852bc97472d0b02cd4945ee3af7485427
ruanvalente <ruanopensuselinux@gmail.com>
2025-11-05 10:45:57 -0300

    feat: config SEO

--- src/app/components/ui/issue-details/issue-details.ts
+++ src/app/components/ui/issue-details/issue-details.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IssuesService, Issue } from '../../../services/issues.service';
 import { Loading } from '../../shared/loading/loading';
+import { SeoService } from '../../../services/seo.service';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-issue-details',
@@ -15,6 +17,7 @@ export class IssueDetails {
   private route = inject(ActivatedRoute);
   private router = inject(Router);
   private issuesService = inject(IssuesService);
+  private seoService = inject(SeoService);
 
   issue = signal<Issue | null>(null);
   loading = signal(true);
@@ -22,6 +25,7 @@ export class IssueDetails {
   private owner = signal<string>('');
   private repo = signal<string>('');
   private issueNumber = signal<number>(0);
+  private readonly baseUrl = environment.app.baseURL;
 
   constructor() {
     const params = this.route.snapshot.params;
@@ -31,10 +35,24 @@ export class IssueDetails {
 
     effect(() => {
       this.loading.set(true);
+
       this.issuesService.getIssue(this.owner(), this.repo(), this.issueNumber()).subscribe({
         next: (issue) => {
           this.issue.set(issue);
           this.loading.set(false);
+
+          const title = `Issue #${issue.number}: ${issue.title} | ${this.repo()} - ${this.owner()}`;
+          const description = issue.body?.slice(0, 160) || `Detalhes do issue #${issue.number}`;
+          const url = `${this.baseUrl}/repository/${this.owner()}/${this.repo()}/issues/${
+            issue.number
+          }`;
+
+          this.seoService.updateTags({
+            title,
+            description,
+            url,
+            image: issue.user?.avatar_url || '',
+          });
         },
         error: () => {
           this.loading.set(false);
